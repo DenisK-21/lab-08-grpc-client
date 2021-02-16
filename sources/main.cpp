baab8e7dd856cddc18de97643f81b0a252c850a3
Denisk-21 <cudinov.denis2016@yandex.ru>
2021-02-17 01:40:05 +0300

    First

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <memory>
 #include <string>
+
+#include <nlohmann/json.hpp>
 
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
@@ -11,21 +14,23 @@
 
 using grpc::ClientContext;
 using grpc::Status;
-using echo::HelloRequest;
-using echo::HelloResponse;
-using echo::Echo;
+using suggest::SuggestRequest;
+using suggest::SuggestAnswer;
+using suggest::SuggestResponse;
+using suggest::Suggest;
 
 int main(int argc, char** argv) {
 
   auto channel = grpc::CreateChannel("0.0.0.0:9090", grpc::InsecureChannelCredentials());
-  auto client = Echo::NewStub(channel);
-  HelloResponse response;
+  auto client = suggestService::NewStub(channel);
+  SuggestResponse response;
   ClientContext context;
-  HelloRequest request;
-  request.set_data("world");
-  Status status = client->Hello(&context, request, &response);
+  SuggestRequest request;
+  request.set_data(argv[1]);
+  Status status = client->Query(&context, request, &response);
 
   if (status.ok()) {
+
     std::cout << response.data() << std::endl;
   } else {
     return -1;
